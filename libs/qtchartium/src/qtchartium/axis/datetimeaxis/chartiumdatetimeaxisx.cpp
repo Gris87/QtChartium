b2432c25775ac53969fbe36ed649786e8a8e1651
Gris87 <Gris87@yandex.ru>
2025-12-01 08:44:51 +0300

    Sources

--- libs/qtchartium/src/qtchartium/axis/datetimeaxis/chartiumdatetimeaxisx.cpp
+++ libs/qtchartium/src/qtchartium/axis/datetimeaxis/chartiumdatetimeaxisx.cpp
@@ -1,5 +1,7 @@
 #include "src/qtchartium/axis/datetimeaxis/chartiumdatetimeaxisx.h"
 
+#include "src/qtchartium/ichartiumpresenter.h"
+
 
 
 ChartiumDateTimeAxisX::ChartiumDateTimeAxisX(
@@ -8,6 +10,10 @@ ChartiumDateTimeAxisX::ChartiumDateTimeAxisX(
     IChartiumDateTimeAxisX(axis, presenter, item, parent),
     mAxis(axis)
 {
+    // clang-format off
+    connect(mAxis, SIGNAL(tickCountChanged(int)),  this, SLOT(handleTickCountChanged(int)));
+    connect(mAxis, SIGNAL(formatChanged(QString)), this, SLOT(handleFormatChanged(QString)));
+    // clang-format on
 }
 
 ChartiumDateTimeAxisX::~ChartiumDateTimeAxisX()
@@ -16,24 +22,133 @@ ChartiumDateTimeAxisX::~ChartiumDateTimeAxisX()
 
 QSizeF ChartiumDateTimeAxisX::sizeHint(Qt::SizeHint which, const QSizeF& constraint) const
 {
-    return QSizeF();
+    Q_UNUSED(constraint);
+
+    QSizeF sh;
+
+    QSizeF            base      = IChartiumDateTimeAxisX::sizeHint(which, constraint);
+    const QStringList ticksList = createDateTimeLabels(min(), max(), mAxis->tickCount(), mAxis->format());
+
+    // Width of horizontal axis sizeHint indicates the maximum distance labels can extend past
+    // first and last ticks. Base width is irrelevant.
+    qreal width  = 0;
+    qreal height = 0;
+
+    if (ticksList.empty())
+    {
+        return sh;
+    }
+
+    switch (which)
+    {
+        case Qt::MinimumSize:
+        {
+            if (labelsVisible())
+            {
+                QRectF boundingRect =
+                    presenter()->textBoundingRect(axis()->labelsFont(), QStringLiteral("..."), axis()->labelsAngle());
+                width  = boundingRect.width() / 2.0;
+                height = boundingRect.height() + labelPadding() + base.height() + 1.0;
+            }
+            else
+            {
+                width  = 0;
+                height = base.height() + 1.0;
+            }
+
+            sh = QSizeF(width, height);
+
+            break;
+        }
+        case Qt::PreferredSize:
+        {
+            if (labelsVisible())
+            {
+                qreal labelHeight = 0.0;
+                qreal firstWidth  = -1.0;
+
+                for (const QString& s : ticksList)
+                {
+                    QRectF rect = presenter()->textBoundingRect(axis()->labelsFont(), s, axis()->labelsAngle());
+                    labelHeight = qMax(rect.height(), labelHeight);
+                    width       = rect.width();
+
+                    if (firstWidth < 0.0)
+                    {
+                        firstWidth = width;
+                    }
+                }
+
+                height = labelHeight + labelPadding() + base.height() + 1.0;
+                width  = qMax(width, firstWidth) / 2.0;
+            }
+            else
+            {
+                height = base.height() + 1.0;
+                width  = 0;
+            }
+
+            sh = QSizeF(width, height);
+
+            break;
+        }
+        default:
+            break;
+    }
+
+    return sh;
 }
 
 QList<qreal> ChartiumDateTimeAxisX::calculateLayout() const
 {
-    QList<qreal> res;
+    int tickCount = mAxis->tickCount();
 
-    return res;
+    Q_ASSERT(tickCount >= 2);
+
+    QList<qreal> points;
+    points.resize(tickCount);
+
+    const QRectF& gridRect = gridGeometry();
+    const qreal   deltaX   = gridRect.width() / (qreal(tickCount) - 1.0);
+
+    for (int i = 0; i < tickCount; ++i)
+    {
+        points[i] = qreal(i) * deltaX + gridRect.left();
+    }
+
+    return points;
 }
 
 void ChartiumDateTimeAxisX::updateGeometry()
 {
+    const QList<qreal>& layout = IChartiumDateTimeAxisX::layout();
+
+    if (layout.isEmpty())
+    {
+        return;
+    }
+
+    setLabels(createDateTimeLabels(min(), max(), layout.size(), mAxis->format()));
+    IChartiumDateTimeAxisX::updateGeometry();
+    updateLabelsDateTimes();
 }
 
 void ChartiumDateTimeAxisX::handleTickCountChanged(int tick)
 {
+    QGraphicsLayoutItem::updateGeometry();
+
+    if (presenter())
+    {
+        presenter()->layout()->invalidate();
+    }
 }
 
 void ChartiumDateTimeAxisX::handleFormatChanged(const QString& format)
 {
+    QGraphicsLayoutItem::updateGeometry();
+
+    if (presenter())
+    {
+        presenter()->layout()->invalidate();
+    }
 }
--- libs/qtchartium/src/qtchartium/axis/datetimeaxis/chartiumdatetimeaxisy.cpp
+++ libs/qtchartium/src/qtchartium/axis/datetimeaxis/chartiumdatetimeaxisy.cpp
@@ -1,5 +1,7 @@
 #include "src/qtchartium/axis/datetimeaxis/chartiumdatetimeaxisy.h"
 
+#include "src/qtchartium/ichartiumpresenter.h"
+
 
 
 ChartiumDateTimeAxisY::ChartiumDateTimeAxisY(
@@ -8,6 +10,10 @@ ChartiumDateTimeAxisY::ChartiumDateTimeAxisY(
     IChartiumDateTimeAxisY(axis, presenter, item, parent),
     mAxis(axis)
 {
+    // clang-format off
+    connect(mAxis, SIGNAL(tickCountChanged(int)),  this, SLOT(handleTickCountChanged(int)));
+    connect(mAxis, SIGNAL(formatChanged(QString)), this, SLOT(handleFormatChanged(QString)));
+    // clang-format on
 }
 
 ChartiumDateTimeAxisY::~ChartiumDateTimeAxisY()
@@ -16,24 +22,131 @@ ChartiumDateTimeAxisY::~ChartiumDateTimeAxisY()
 
 QSizeF ChartiumDateTimeAxisY::sizeHint(Qt::SizeHint which, const QSizeF& constraint) const
 {
-    return QSizeF();
+    Q_UNUSED(constraint);
+
+    QSizeF sh;
+
+    QSizeF            base      = IChartiumDateTimeAxisY::sizeHint(which, constraint);
+    const QStringList ticksList = createDateTimeLabels(min(), max(), mAxis->tickCount(), mAxis->format());
+    qreal             width     = 0;
+
+    // Height of vertical axis sizeHint indicates the maximum distance labels can extend past
+    // first and last ticks. Base height is irrelevant.
+    qreal height = 0;
+
+    if (ticksList.empty())
+    {
+        return sh;
+    }
+
+    switch (which)
+    {
+        case Qt::MinimumSize:
+        {
+            if (labelsVisible())
+            {
+                QRectF boundingRect =
+                    presenter()->textBoundingRect(axis()->labelsFont(), QStringLiteral("..."), axis()->labelsAngle());
+                width  = boundingRect.width() + labelPadding() + base.width() + 1.0;
+                height = boundingRect.height() / 2.0;
+            }
+            else
+            {
+                width  = base.width() + 1.0;
+                height = 0;
+            }
+
+            sh = QSizeF(width, height);
+
+            break;
+        }
+        case Qt::PreferredSize:
+        {
+            if (labelsVisible())
+            {
+                qreal labelWidth  = 0.0;
+                qreal firstHeight = -1.0;
+
+                for (const QString& s : ticksList)
+                {
+                    QRectF rect = presenter()->textBoundingRect(axis()->labelsFont(), s, axis()->labelsAngle());
+                    labelWidth  = qMax(rect.width(), labelWidth);
+                    height      = rect.height();
+
+                    if (firstHeight < 0.0)
+                    {
+                        firstHeight = height;
+                    }
+                }
+                width  = labelWidth + labelPadding() + base.width() + 2.0; //two pixels of tolerance
+                height = qMax(height, firstHeight) / 2.0;
+            }
+            else
+            {
+                width  = base.width() + 2.0;
+                height = 0;
+            }
+
+            sh = QSizeF(width, height);
+            break;
+        }
+        default:
+            break;
+    }
+
+    return sh;
 }
 
 QList<qreal> ChartiumDateTimeAxisY::calculateLayout() const
 {
-    QList<qreal> res;
+    const int tickCount = mAxis->tickCount();
 
-    return res;
+    Q_ASSERT(tickCount >= 2);
+
+    QList<qreal> points;
+    points.resize(tickCount);
+
+    const QRectF& gridRect = gridGeometry();
+    const qreal   deltaY   = gridRect.height() / (qreal(tickCount) - 1.0);
+
+    for (int i = 0; i < tickCount; ++i)
+    {
+        points[i] = qreal(i) * -deltaY + gridRect.bottom();
+    }
+
+    return points;
 }
 
 void ChartiumDateTimeAxisY::updateGeometry()
 {
+    const QList<qreal>& layout = IChartiumDateTimeAxisY::layout();
+
+    if (layout.isEmpty())
+    {
+        return;
+    }
+
+    setLabels(createDateTimeLabels(min(), max(), layout.size(), mAxis->format()));
+    IChartiumDateTimeAxisY::updateGeometry();
+    updateLabelsDateTimes();
 }
 
 void ChartiumDateTimeAxisY::handleTickCountChanged(int tick)
 {
+    QGraphicsLayoutItem::updateGeometry();
+
+    if (presenter())
+    {
+        presenter()->layout()->invalidate();
+    }
 }
 
 void ChartiumDateTimeAxisY::handleFormatChanged(const QString& format)
 {
+    QGraphicsLayoutItem::updateGeometry();
+
+    if (presenter())
+    {
+        presenter()->layout()->invalidate();
+    }
 }
