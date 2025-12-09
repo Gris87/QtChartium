0fde39381f9db114c4a747842b6015b44beea61b
Gris87 <Gris87@yandex.ru>
2025-12-09 19:33:55 +0300

    Sources

--- libs/qtchartium/src/qtchartium/legend/chartiumlegendmarker.cpp
+++ libs/qtchartium/src/qtchartium/legend/chartiumlegendmarker.cpp
@@ -1,14 +1,27 @@
 #include "src/qtchartium/legend/chartiumlegendmarker.h"
 
+#include "src/qtchartium/charts/ichartiumseries.h"
+#include "src/qtchartium/legend/chartiumlegendmarkeritem.h"
+#include "src/qtchartium/legend/ichartiumlegendlayout.h"
+
 
 
 ChartiumLegendMarker::ChartiumLegendMarker(IChartiumLegend* legend, QObject* parent) :
-    IChartiumLegendMarker(parent)
+    IChartiumLegendMarker(parent),
+    m_item(new ChartiumLegendMarkerItem(this, legend->presenter())),
+    m_legend(legend),
+    m_customLabel(),
+    m_customBrush(),
+    m_customPen()
 {
+    m_item->setVisible(series()->isVisible());
+
+    connect(legend, SIGNAL(markerShapeChanged()), this, SLOT(handleShapeChange()));
 }
 
 ChartiumLegendMarker::~ChartiumLegendMarker()
 {
+    delete m_item;
 }
 
 IChartiumLegendMarker::LegendMarkerType ChartiumLegendMarker::type()
@@ -18,65 +31,101 @@ IChartiumLegendMarker::LegendMarkerType ChartiumLegendMarker::type()
 
 QString ChartiumLegendMarker::label() const
 {
-    return "";
+    return m_item->label();
 }
 
 void ChartiumLegendMarker::setLabel(const QString& label)
 {
+    if (label.isEmpty())
+    {
+        m_customLabel = false;
+    }
+    else
+    {
+        m_customLabel = true;
+        m_item->setLabel(label);
+    }
 }
 
 QBrush ChartiumLegendMarker::labelBrush() const
 {
-    return QBrush();
+    return m_item->labelBrush();
 }
 
 void ChartiumLegendMarker::setLabelBrush(const QBrush& brush)
 {
+    m_item->setLabelBrush(brush);
 }
 
 QFont ChartiumLegendMarker::font() const
 {
-    return QFont();
+    return m_item->font();
 }
 
 void ChartiumLegendMarker::setFont(const QFont& font)
 {
+    m_item->setFont(font);
 }
 
 QPen ChartiumLegendMarker::pen() const
 {
-    return QPen();
+    return m_item->pen();
 }
 
 void ChartiumLegendMarker::setPen(const QPen& pen)
 {
+    if (pen == QPen(Qt::NoPen))
+    {
+        m_customPen = false;
+    }
+    else
+    {
+        m_customPen = true;
+        m_item->setPen(pen);
+    }
 }
 
 QBrush ChartiumLegendMarker::brush() const
 {
-    return QBrush();
+    return m_item->brush();
 }
 
 void ChartiumLegendMarker::setBrush(const QBrush& brush)
 {
+    if (brush == QBrush(Qt::NoBrush))
+    {
+        m_customBrush = false;
+    }
+    else
+    {
+        m_customBrush = true;
+        m_item->setBrush(brush);
+    }
 }
 
 bool ChartiumLegendMarker::isVisible() const
 {
-    return false;
+    return m_item->isVisible();
 }
 
 void ChartiumLegendMarker::setVisible(bool visible)
 {
+    m_item->setVisible(visible);
 }
 
 IChartiumLegend::MarkerShape ChartiumLegendMarker::shape() const
 {
-    return IChartiumLegend::MarkerShapeDefault;
+    return m_item->markerShape();
 }
 
 void ChartiumLegendMarker::setShape(IChartiumLegend::MarkerShape shape)
 {
+    if (shape != m_item->markerShape())
+    {
+        m_item->setMarkerShape(shape);
+        handleShapeChange();
+        emit shapeChanged();
+    }
 }
 
 IChartiumSeries* ChartiumLegendMarker::series()
@@ -96,10 +145,20 @@ IChartiumLegendMarkerItem* ChartiumLegendMarker::item() const
 
 void ChartiumLegendMarker::invalidateLegend()
 {
+    m_item->updateGeometry();
+    m_legend->layout()->invalidate();
 }
 
 void ChartiumLegendMarker::invalidateAllItems()
 {
+    QList<IChartiumLegendMarker*> markers = m_legend->markers();
+
+    for (int i = 0; i < markers.size(); i++)
+    {
+        markers.at(i)->item()->updateGeometry();
+    }
+
+    m_legend->layout()->invalidate();
 }
 
 IChartiumLegend* ChartiumLegendMarker::legend() const
@@ -113,4 +172,6 @@ void ChartiumLegendMarker::updated()
 
 void ChartiumLegendMarker::handleShapeChange()
 {
+    m_item->updateMarkerShapeAndSize();
+    m_legend->layout()->invalidate();
 }
